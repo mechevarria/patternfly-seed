916f0a7e90968fccbe9084dfb53fd0d0c87136df
Mike Echevarria <mike.echevarria@gmail.com>
2017-07-06 10:01:34 -0400

    turn navigation menu into component

---
+++ app/components/nav/nav-component.js
@@ -0,0 +1,9 @@
+(function() {
+    'use strict';
+
+    angular.module('patternfly.app')
+        .component('navMenu', {
+            templateUrl: 'app/components/nav/nav.html',
+            controller: 'NavCtrl'
+        });
+})();
